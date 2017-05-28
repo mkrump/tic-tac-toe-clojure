c0420c3c9beb8083bec06c7ff2333804450d8fa9
mkrump <mkrump@8thlight.com>
2017-05-28 16:27:22 -0500

    Simplified minimax

--- src/tictactoe/computer_minimax_player.clj
+++ src/tictactoe/computer_minimax_player.clj
@@ -2,32 +2,26 @@
   (:require [tictactoe.board :as board])
   (:require [tictactoe.detect-board-state :as detect-board-state]))
 
-(defn- score-function [board gridsize player depth]
-  (let [winner (detect-board-state/winner board gridsize)]
+(defn score [board player depth]
+  (let [winner (detect-board-state/winner (:board-contents board) (:gridsize board))]
     (if (not= 0 winner)
       (/ (* player winner) depth)
       0)))
 
-(defn- minimax-move-score [board current-player depth]
-  (loop [candidate-moves (board/open-squares board)
-         best-score (Double/NEGATIVE_INFINITY)
-         depth 1]
-      (if (true? (detect-board-state/game-over? (:board-contents board) (:gridsize board)))
-        (score-function (:board-contents board) (:gridsize board) current-player depth)
-        (if (seq candidate-moves)
-          (do
-            (let [move (first candidate-moves)
-                  updated-board (board/make-move board move current-player)
-                  score (* -1 (minimax-move-score updated-board (* -1 current-player) (inc depth)))]
-              (if (> score best-score)
-                (recur (rest candidate-moves) score (inc depth))
-                (recur (rest candidate-moves) best-score (inc depth)))))
-          best-score))))
+(defn next-board-seq [board player]
+  (let [open-squares (board/open-squares board)]
+    (map #(board/make-move board % player) open-squares)))
+
+(defn minimax-move-score [board player depth]
+  (if (detect-board-state/game-over? (:board-contents board) (:gridsize board))
+    (score board player depth)
+    (let [board-seq (next-board-seq board player)]
+      (reduce max
+        (map #(- (minimax-move-score % (- player) (inc depth))) board-seq)))))
 
 (defn- minimax-score-moves [board current-player]
-  (doall
-    (pmap #(vector % (* -1 (minimax-move-score (board/make-move board %1 %2) (* -1 %2) 1)))
-       (board/open-squares board) (iterate identity current-player))))
+  (pmap #(vector % (- (minimax-move-score (board/make-move board %1 %2) (* -1 %2) 1)))
+     (board/open-squares board) (iterate identity current-player)))
 
 (defn minimax-move [board current-player]
   ((apply max-key #(get % 1) (minimax-score-moves board current-player)) 0))
