2373a75f75b541e531afc79e1f4192ef019ce064
mkrump <mkrump@8thlight.com>
2017-05-29 15:49:14 -0500

    Randomize computer moves if all have equal score

--- src/tictactoe/computer_minimax_ab_player.clj
+++ src/tictactoe/computer_minimax_ab_player.clj
@@ -9,19 +9,21 @@
       (/ (* player winner) depth)
       0)))
 
-(defn next-board-seq [board player]
+(defn- next-board-seq [board player]
   (let [open-squares (board/open-squares board)]
     (map #(board/make-move board % player) open-squares)))
 
-(defn ab-pruning [[player depth alpha beta best-score] node]
-    (let [node-score (- (minimax-ab-move-score node (- player) (inc depth) (- beta) (- alpha)))
-          updated-alpha (max alpha node-score)
-          updated-best-score (max best-score node-score)]
+(defn- ab-pruning [[player depth alpha beta best-score] board]
+    (let [move-score (- (minimax-ab-move-score board (- player) (inc depth) (- beta) (- alpha)))
+          updated-alpha (max alpha move-score)
+          updated-best-score (max best-score move-score)]
       (if (>= updated-alpha beta)
+        ;Short circuit reduce w/ reduced when alpha >= beta
+        ;https://stackoverflow.com/questions/7491360/how-do-you-return-from-a-function-early-in-clojure
         (reduced [player depth alpha beta updated-best-score])
         [player depth updated-alpha beta updated-best-score])))
 
-(defn minimax-ab-move-score [board player depth alpha beta]
+(defn- minimax-ab-move-score [board player depth alpha beta]
   (let [best-score (Double/NEGATIVE_INFINITY)
         board-seq (next-board-seq board player)]
     (if (detect-board-state/game-over? (:board-contents board) (:gridsize board))
@@ -35,11 +37,12 @@
           (- (minimax-ab-move-score (board/make-move board % player) (- player) 1 (Double/NEGATIVE_INFINITY) (Double/POSITIVE_INFINITY))))
        (board/open-squares board)))
 
-(defn minimax-move [board current-player]
-  ((apply max-key #(get % 1) (minimax-score-moves board current-player)) 0))
+(defn- minimax-move [board current-player]
+  (let [moves (minimax-score-moves board current-player)
+        max-score ((apply max-key #(get % 1) moves) 1)
+        best-moves (filter #(= max-score (get % 1)) moves)]
+       ((rand-nth best-moves) 0)))
 
 (defn computer-minimax-ab-player [marker]
   {:marker marker :move minimax-move})
 
-;Short circuit reduce w/ reduced
-;;https://stackoverflow.com/questions/7491360/how-do-you-return-from-a-function-early-in-clojure
