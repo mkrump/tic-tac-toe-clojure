23221a21a5f67310849763517de38223dbc7bfce
mkrump <mkrump@8thlight.com>
2017-06-07 10:41:45 -0500

    Updated board-contents to work better w/ api

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject tictactoe "0.1.0"
+(defproject org.clojars.mkrump/tictactoe "0.1.0"
   :description "TicTacToe"
   :license {:name "MIT"}
+  :url "https://github.com/mkrump/tic-tac-toe-clojure"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot tictactoe.main
   :profiles {:uberjar {:aot :all}})
