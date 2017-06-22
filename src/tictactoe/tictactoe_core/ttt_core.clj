0cd5c1439e6fd649eca78001b25d2163b8ee122a
mkrump <mkrump@8thlight.com>
2017-06-22 09:20:26 -0500

    Derive current player from board
    - Removed switch player and instead derive current player from board
    - https://chicagotechgrowhouse.atlassian.net/browse/MKA-85

--- src/tictactoe/tictactoe_core/ttt_core.clj
+++ src/tictactoe/tictactoe_core/ttt_core.clj
@@ -8,16 +8,19 @@
    false {:status :valid-move :message "Success"}
    true  {:status :occupied :message "Square occupied"}})
 
-(defn switch-player [player]
-  (* -1 player))
+(defn determine-current-player [board]
+  (let [open-squares-count (count (board/open-squares board))]
+    (cond
+      (even? (:gridsize board)) (if (even? open-squares-count) -1 1)
+      (odd? (:gridsize board)) (if (odd? open-squares-count) -1 1))))
 
 (defn initial-game-state [gridsize]
   (let [board (board/generate-board gridsize)]
-    {:board board
+    {:board          board
      :current-player -1
-     :is-tie false
-     :winner 0
-     :game-over false}))
+     :is-tie         false
+     :winner         0
+     :game-over      false}))
 
 (defn move-status [game-state move]
   (let [{:keys [board winner is-tie current-player]} game-state
@@ -30,7 +33,7 @@
         {:keys [board-contents gridsize]} updated-board
         updated-winner (detect-board-state/winner board-contents gridsize)
         updated-is-tie (detect-board-state/tie? board-contents gridsize)
-        updated-current-player (switch-player current-player)
+        updated-current-player (determine-current-player updated-board)
         updated-game-over (detect-board-state/game-over? board-contents gridsize)]
     {:board          updated-board
      :winner         updated-winner
--- test/tictactoe/tictactoe_core/ttt_core_test.clj
+++ test/tictactoe/tictactoe_core/ttt_core_test.clj
@@ -19,13 +19,26 @@
    :winner         0
    :game-over      false})
 
-(deftest switch-player-player1-test
-  (testing "A switching player 1 results in player 2"
-    (is (= -1 (switch-player 1)))))
 
-(deftest switch-player-player2-test
-  (testing "A switching player 2 results in player 1"
-    (is (= 1 (switch-player -1)))))
+(deftest current-player-turn-empty-test
+  (testing "A correct current player turn is derived from a board"
+    (let [board {:board-contents [0 0 0 0 0 0 0 0 0] :gridsize 3}]
+      (is (= -1 (determine-current-player board))))))
+
+(deftest current-player-turn-non-empty-test
+  (testing "A correct current player turn is derived from a board"
+    (let [board {:board-contents [-1 1 -1 0 0 0 0 0 0] :gridsize 3}]
+      (is (= 1 (determine-current-player board))))))
+
+(deftest current-player-turn-4x4-empty-test
+  (testing "A correct current player turn is derived from a board"
+    (let [board {:board-contents [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0] :gridsize 4}]
+      (is (= -1 (determine-current-player board))))))
+
+(deftest current-player-turn-4x4-non-empty-test
+  (testing "A correct current player turn is derived from a board"
+    (let [board {:board-contents [-1 0 0 0 1 0 0 -1 0 0 0 0 0 0 0 0] :gridsize 4}]
+      (is (= 1 (determine-current-player board))))))
 
 (deftest square-occupied-test
   (testing "An occupied square should return :occupied"
@@ -42,7 +55,7 @@
     (is (= {:status :valid-move, :message "Success"}
            (move-status current-game-state 0)))))
 
-(deftest update-game-state-invalid-test
+(deftest update-game-state-no-win-test
   (testing "Next game-state is returned"
     (let
       [current-game-state
@@ -59,22 +72,22 @@
         :game-over      false}]
       (is (= expected-game-state (update-game-state current-game-state 0))))))
 
-(deftest update-game-state-invalid-test
+(deftest update-game-state-win-test
   (testing "Next game-state is returned"
     (let
       [current-game-state
-       {:board          {:board-contents [0 1 1 0 0 0 0 0 0] :gridsize 3}
+       {:board          {:board-contents [1 0 -1 1 -1 -1 0 0 0] :gridsize 3}
         :current-player 1
         :is-tie         false
         :winner         0
         :game-over      false}
        expected-game-state
-       {:board          {:board-contents [1 1 1 0 0 0 0 0 0] :gridsize 3}
+       {:board          {:board-contents [1 0 -1 1 -1 -1 1 0 0] :gridsize 3}
         :current-player -1
         :is-tie         false
         :winner         1
         :game-over      true}]
-      (is (= expected-game-state (update-game-state current-game-state 0))))))
+      (is (= expected-game-state (update-game-state current-game-state 6))))))
 
 (deftest computer-move-test
   (testing "For a given game state computer move is returned"
