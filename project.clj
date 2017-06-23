09b90592f19768ddb3b85d874b45244a2455e304
mkrump <mkrump@8thlight.com>
2017-06-23 09:35:12 -0500

    Version 0.1.2 clojars

--- project.clj
+++ project.clj
@@ -1,11 +1,15 @@
-(defproject org.clojars.mkrump/tictactoe "0.1.1"
+(defproject org.clojars.mkrump/tictactoe "0.1.3-SNAPSHOT"
   :description "TicTacToe"
   :license {:name "MIT"}
   :url "https://github.com/mkrump/tic-tac-toe-clojure"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot tictactoe.main
   :profiles {:uberjar {:aot :all}}
-  :repositories [["snapshots"
+  :repositories [["releases"
+                  {:url "https://clojars.org/repo/"
+                   :username [:gpg :env/cj_username]
+                   :password [:gpg :env/cj_password]}]
+                 ["snapshots"
                   {:url "https://clojars.org/repo/"
                    :username [:gpg :env/cj_username]
                    :password [:gpg :env/cj_password]}]])
